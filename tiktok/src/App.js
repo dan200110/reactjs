846bdbe0171c654cedea6c661dbf005a4211bec4
Nguyen Tien Dan <nguyen.tien.dan@sun-asterisk.com>
2023-11-22 13:19:30 +0700

    useState

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
+const orders = [100, 200, 300]
 function App() {
+  const [counter, setCounter] = useState(() => {
+    return orders.reduce((total, cur) => total + cur)
+  })
+  const handleIncrease = () => {
+    setCounter(prevState => prevState + 1)
+  }
+
+  const [info, setInfo] = useState({
+    name: 'Nguyen Van A',
+    age: 15,
+    address: 'ha noi'
+  })
+
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      bio: 'Add more detail'
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 20}}>
+
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleUpdate}>Update</button>
+      <h1>{JSON.stringify(info)}</h1>
     </div>
   );
 }
 
 export default App;
+
+// Luu y:
+// - Component được re-render sau khi 'setState'
+// - Initial state với callback?
+// - Set state với callback?
+// - Initial state với callback?
+// - Set state là thay thế state bằng giá trị mới
