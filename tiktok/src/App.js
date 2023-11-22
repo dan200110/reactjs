177e6a58bae3bfb26ed63957b78559927010ec6f
Nguyen Tien Dan <nguyen.tien.dan@sun-asterisk.com>
2023-11-22 14:54:05 +0700

    save to local storage

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -2,61 +2,36 @@ import { useState } from 'react'
 
 
 function App() {
-  const gifts = [
-    'gift 1', 'gitf2', 'gift3'
-  ]
-
-  const [gift, setGift] = useState()
-
-  const randomGift = () => {
-    let index = Math.floor(Math.random() * gifts.length)
-    setGift(gifts[index])
-  }
-
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    console.log(storageJobs);
+    return storageJobs
+  })
   const handleSubmit = () => {
-    console.log({name, email});
+    setJobs(prev => {
+      const jsonJobs = JSON.stringify([...prev, job])
+
+      // Save to local storage
+      localStorage.setItem('jobs', jsonJobs)
+      return [...prev, job]
+    })
+    setJob('')
   }
-  
-  const courses = [
-    { id: 1, name: 'course 1'},
-    { id: 2, name: 'course 2'},
-    { id: 3, name: 'course 3'}
-  ]
-  const [checked, setChecked] = useState()
   return (
     <div className="App" style={{padding: 20}}>
-      <h1>{gift || 'No reward yet'}</h1>
-      <button onClick={randomGift}>Get reward</button>
-
-      <div style={{padding: 50}}>
-        <input
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-
-        <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-
-        <button onClick={handleSubmit}>Register</button>
-      </div>
-
-      <div style={{padding: 100}}>
-        {courses.map(course => {
-          <div key={course.id}>
-            <input
-              value={course.name}
-              onChange={() => {setChecked(course.id)}}
-            />
-            <h1>{course.name}</h1>
-          </div>
-        })}
-      </div>
-
+      <input
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+        <li key={index}>
+          {job}
+        </li>
+        ))}
+      </ul>
     </div>
   );
 }
