c90706de777692c0e06ce3ddad01a68b5b0dcec4
Nguyen Tien Dan <nguyen.tien.dan@sun-asterisk.com>
2023-11-22 14:06:55 +0700

    two-way bindding

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,42 +1,64 @@
 import { useState } from 'react'
 
-const orders = [100, 200, 300]
+
 function App() {
-  const [counter, setCounter] = useState(() => {
-    return orders.reduce((total, cur) => total + cur)
-  })
-  const handleIncrease = () => {
-    setCounter(prevState => prevState + 1)
+  const gifts = [
+    'gift 1', 'gitf2', 'gift3'
+  ]
+
+  const [gift, setGift] = useState()
+
+  const randomGift = () => {
+    let index = Math.floor(Math.random() * gifts.length)
+    setGift(gifts[index])
   }
 
-  const [info, setInfo] = useState({
-    name: 'Nguyen Van A',
-    age: 15,
-    address: 'ha noi'
-  })
 
-  const handleUpdate = () => {
-    setInfo({
-      ...info,
-      bio: 'Add more detail'
-    })
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const handleSubmit = () => {
+    console.log({name, email});
   }
+  
+  const courses = [
+    { id: 1, name: 'course 1'},
+    { id: 2, name: 'course 2'},
+    { id: 3, name: 'course 3'}
+  ]
+  const [checked, setChecked] = useState()
   return (
     <div className="App" style={{padding: 20}}>
+      <h1>{gift || 'No reward yet'}</h1>
+      <button onClick={randomGift}>Get reward</button>
 
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleUpdate}>Update</button>
-      <h1>{JSON.stringify(info)}</h1>
+      <div style={{padding: 50}}>
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <button onClick={handleSubmit}>Register</button>
+      </div>
+
+      <div style={{padding: 100}}>
+        {courses.map(course => {
+          <div key={course.id}>
+            <input
+              value={course.name}
+              onChange={() => {setChecked(course.id)}}
+            />
+            <h1>{course.name}</h1>
+          </div>
+        })}
+      </div>
+
     </div>
   );
 }
 
 export default App;
-
-// Luu y:
-// - Component được re-render sau khi 'setState'
-// - Initial state với callback?
-// - Set state với callback?
-// - Initial state với callback?
-// - Set state là thay thế state bằng giá trị mới
